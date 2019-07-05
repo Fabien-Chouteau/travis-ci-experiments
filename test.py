be1c12adaef2b45ec55c1abf88b5d1d3a3b5049a
Fabien Chouteau <chouteau@adacore.com>
2019-07-05 16:14:23 +0200

    test

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 import sys
+import toml
 
 env_list = ['TRAVIS_PULL_REQUEST_SLUG',
             'TRAVIS_REPO_SLUG',
@@ -17,9 +18,23 @@ for elt in env_list:
     else:
         print("%s not defined" % elt)
 
-if 'TRAVIS_PULL_REQUEST_SLUG' in os.environ:
-    username, _ = os.environ['TRAVIS_PULL_REQUEST_SLUG'].split('/', 1)
-    print("PR username: %s" % str(username))
+def check_username(username, file):
+    with open(file, 'r', encoding='utf-8') as fp:
+        content = toml.load(fp)
+        if 'general' not in content:
+            return False
+        elif 'github_usernames' not in content.get("general"):
+            return False
+        else:
+            return username in content.get("general").get('github_usernames')
+
+def username():
+    if 'TRAVIS_PULL_REQUEST_SLUG' in os.environ:
+        username, _ = os.environ['TRAVIS_PULL_REQUEST_SLUG'].split('/', 1)
+        return username
+    else:
+        return "_NO_USERNAME_"
+
 
 if 'TRAVIS_COMMIT_RANGE' in os.environ:
     output = subprocess.check_output(['git', 'show', '--pretty=', '--name-only', os.environ['TRAVIS_COMMIT_RANGE']])
@@ -31,5 +46,15 @@ if 'TRAVIS_COMMIT_RANGE' in os.environ:
 
     if len(file_list) < 0:
         print("No file modified in this PR???")
+        os.sys.exit(1)
     elif len(file_list) > 0:
         print("More than one file modified in this PR")
+        os.sys.exit(1)
+    else:
+        if check_username(username(), file_list[0]):
+            print("PR author owns the file")
+            os.sys.exit(0)
+        else:
+            print("PR author doesn't own the file")
+            os.sys.exit(1)
+
