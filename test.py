370f6e035d3ba5c7c7b5649ed693c0a5e55454e7
Fabien Chouteau <chouteau@adacore.com>
2019-07-05 15:49:18 +0200

    test

--- test.py
+++ test.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python3
 
 import os
+import subprocess
+import sys
 
-list = ['TRAVIS_PULL_REQUEST_SLUG',
-        'TRAVIS_REPO_SLUG',
-        'TRAVIS_BRANCH',
-        'TRAVIS_COMMIT',
-        'TRAVIS_COMMIT_RANGE',
-        'TRAVIS_EVENT_TYPE']
+env_list = ['TRAVIS_PULL_REQUEST_SLUG',
+            'TRAVIS_REPO_SLUG',
+            'TRAVIS_BRANCH',
+            'TRAVIS_COMMIT',
+            'TRAVIS_COMMIT_RANGE',
+            'TRAVIS_EVENT_TYPE']
 
-for elt in list:
+for elt in env_list:
     if elt in os.environ:
         print("%s -> '%s'" % (elt, os.environ[elt]))
     else:
         print("%s not defined" % elt)
 
-
 if 'TRAVIS_PULL_REQUEST_SLUG' in os.environ:
     username, _ = os.environ['TRAVIS_PULL_REQUEST_SLUG'].split('/', 1)
     print("PR username: %s" % str(username))
+
+if 'TRAVIS_COMMIT_RANGE' in os.environ:
+    output = subprocess.check_output(['git', 'show', '--pretty=', '--name-only', os.environ['TRAVIS_COMMIT_RANGE']])
+    file_list = output.decode(sys.stdout.encoding).split("\n")
+    file_list = [i for i in file_list if i]
+    file_list.sort()
+    file_list = list(set(file_list))
+    print("List of files: %s" % str(file_list))
+
+    if len(file_list) < 0:
+        print("No file modified in this PR???")
+    elif len(file_list) > 0:
+        print("More than one file modified in this PR")
